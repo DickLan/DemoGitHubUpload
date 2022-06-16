1fdb29e497752b5fcb8af4030cb1822649cbb810
Blue <D>
2022-06-15 23:55:25 -0700

    Initial Commit

---
+++ Homework1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework1
+//
+//  Created by mac on 2022/6/15.
+//  Copyright © 2022 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
